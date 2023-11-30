403a1638b31d5e2bf5fe1b275ddfb14d25d25829
mizaeldouglas <mizaeldouglas2@gmail.com>
2023-11-30 14:31:19 -0300

    Aula 2 Concluida

--- app/src/main/java/br/com/mizaeldouglas/orgs/MainActivity.kt
+++ app/src/main/java/br/com/mizaeldouglas/orgs/MainActivity.kt
@@ -2,12 +2,13 @@ package br.com.mizaeldouglas.orgs
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Ola Mundo!!", Toast.LENGTH_SHORT).show()
+        setContentView(R.layout.activity_main)
     }
 }
